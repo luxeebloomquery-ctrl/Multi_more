a624ef5ba75fec1d2d3a1071df91714736b67651
luxeebloomquery-ctrl <luxeebloomquery@gmail.com>
2026-07-23 19:59:43 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,15 @@
 from aiogram import Router
 
+from .start import router as start_router
+from .announce import router as announce_router
+from .broadcast import router as broadcast_router
+from .groups import router as groups_router
+from .schedule import router as schedule_router
+
 router = Router()
 
-from . import start
-from . import announce
-from . import broadcast
-from . import groups
-from . import schedule
+router.include_router(start_router)
+router.include_router(announce_router)
+router.include_router(broadcast_router)
+router.include_router(groups_router)
+router.include_router(schedule_router)
